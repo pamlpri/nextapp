08afa264c6158664c5f81dd2ba5f4ce8d9e71d88
DESKTOP-QC9GMNP\seesun <kid03196@gmail.com>
2023-12-20 10:22:47 +0900

    수정, 삭제 기능 공부

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,7 +1,7 @@
 export default async function Read(props) {
     // 파라미터 값을 가져옴
     const id = props.params.id;
-    const resp = await fetch(`http:localhost:9999/topics/${id}`);
+    const resp = await fetch(`http:localhost:9999/topics/${id}`, { cache:'no-store' });
     const topic = await resp.json();
     return(
         <>
--- src/app/update/[id]/page.js
+++ src/app/update/[id]/page.js
@@ -1,10 +1,31 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
 
-export default function Update() {
-    
+export default function Update(props) {    
     const router = useRouter();
+    /*
+        [형식]
+        const [state, setState] = useState(초기값);
+        useState() : 컴포넌트가 가질 수 있는 상태
+        
+        [형식]
+        useEffect(()=>{});
+        useEffect() : 컴포넌트가 렌더링 될 때마다 특정 작업을 실행할 수 있도록 하는 Hook
+     */
+    const id = props.params.id;
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    useEffect(()=>{
+        fetch(`http://localhost:9999/topics/${id}`)
+            .then(res=>res.json())
+            .then(result=>{
+                // console.log(result);
+                setTitle(result.title);
+                setBody(result.body);
+            })
+    },[]); // [] : 한 번만 실행
     return(     
         <>
             <h2>Update</h2>
@@ -14,11 +35,11 @@ export default function Update() {
                 const body = e.target.body.value;
 
                 const options = {
-                    method: 'POST',
+                    method: 'PATCH',
                     headers: { 'Content-Type' : 'application/json' },
                     body: JSON.stringify({title, body})
                 }
-                fetch(`http://localhost:9999/topics`, options)
+                fetch(`http://localhost:9999/topics/${id}`, options)
                     .then(res=>res.json())
                     .then(result=> {
                         console.log(result);
@@ -27,8 +48,9 @@ export default function Update() {
                         router.refresh(); // 페이지 새로고침
                     })
             }}>
-                <p><input type="text" name="title" /></p>
-                <p><textarea name="body" /></p>
+                {/* onChange() 이벤트를 통해 데이터가 변할 때마다 setter 작업을 해서 value 값에 넣어줌 */}
+                <p><input type="text" name="title" value={title} onChange={e=>{setTitle(e.target.value)}}/></p>
+                <p><textarea name="body" value={body} onChange={e=>setBody(e.target.value)}/></p>
                 <input type="submit" value="update"/>
             </form>
         </>
